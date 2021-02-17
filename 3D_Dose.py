22d58291f21959e5a3271f85a59c0e38e466f064
Simon Gutwein <simon.bonvino@gmail.com>
2021-02-17 11:56:45 +0100

    "Transfer to Git"

---
+++ 3D_Dose.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def dose_distribution_3D(filename):
+
+	file = open(filename)
+	dim = np.array(list(map(int, file.readline().split())))
+	z, y, x = np.array(list(map(float, file.readline().split()))), np.array(list(map(float, file.readline().split()))) ,np.array(list(map(float, file.readline().split())))
+	dose_dat = np.array(list(map(float, file.readline().split())))
+	dose_dat = dose_dat.reshape(dim[2], dim[1],  dim[0])
+
+	return x, y, z, dose_dat
+
+def plot_dose_distribution(dose_3D,x ,y ,z):
+	
+	plt.imshow(dose_3D[:,:,0])
+	#plt.show()
+	pos = 50
+	x_center, y_center = np.where(dose_3D[:,:,pos] == dose_3D[:,:,pos].max())
+	x_center = x_center[0]
+	y_center = y_center[0]
+
+	TDV = dose_3D[x_center,y_center,:]
+	IP = dose_3D[:,y_center,pos]
+	CP = dose_3D[x_center,:,pos]
+
+	plt.subplot(3, 1, 1)
+	plt.plot(z[:-1],TDV[::-1])
+	plt.subplot(3, 1, 2)
+	plt.plot(x[:-1],IP)
+	plt.subplot(3, 1, 3)
+	plt.plot(y[:-1],CP)
+	plt.show()
+	
+filename = "water_phantom_2X2.3ddose"
+
+x, y, z, dose_3D = dose_distribution_3D(filename)
+
+plot_dose_distribution(dose_3D, x, y, z)
---
+++ MLC_Plot.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as ptc
+
+file = open("/Users/simongutwein/Studium/Masterarbeit/Data/MR-Linac_model.egsinp")
+
+i=0
+while True:
+	i += 1
+	if i == 10000:
+		break
+
+	t_line = file.readline()
+	if "LEAFRADIUS, CIL" in t_line:
+		info = ""
+		for i in range(80): 
+			info += file.readline()
+	if "R0LEAF, Z0LEAF" in t_line:
+		JAW_dat = file.readline()
+
+		break
+
+print(info,JAW_dat)
+
+info = np.array(info.replace(',', '').split())
+info = info.astype(float)
+left_MLC = info[0::3]+41.5
+left_offset = -5.4-left_MLC
+right_MLC = info[1::3]- 41.5 #oder abs(info[1::3]- 41.5)
+right_offset = 5.4-right_MLC
+JAW_dat = np.array(JAW_dat.replace(',', '').split())
+JAW_dat = JAW_dat.astype(float)
+JAW = [JAW_dat[0]]
+JAW.append(JAW_dat[1])
+
+ax = plt.barh(range(len(left_MLC)),left_offset,left=left_MLC)
+plt.barh(range(len(left_MLC)),right_offset,left=right_MLC)
+#plt.yticks(np.linspace(0,79,8),np.around(np.linspace(-28.6,28.6,8),decimals=2))
+#plt.xticks(np.linspace(-5.4,5.4,10),np.around(np.linspace(-54,54,10),decimals=2))
+ax = plt.gca()
+ax.add_patch(ptc.Rectangle((-5.4,-0.4),10.8,39.9+JAW[0],facecolor="tab:red",alpha=0.5))
+ax.add_patch(ptc.Rectangle((-5.4,39.5+JAW[1]),10.8,39.9-JAW[1], facecolor="tab:red",alpha=0.5))
+plt.show()
