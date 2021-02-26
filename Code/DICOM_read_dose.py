0f41d58eadebaba27a069148aa8aadff8100dec3
Simon Gutwein <simon.bonvino@gmail.com>
2021-02-26 15:07:39 +0100

    Added functions for MLC and Jaw calculation

--- Code/DICOM_read_dose.py
+++ Code/DICOM_read_dose.py
@@ -4,8 +4,41 @@ import numpy as np
 import matplotlib.patches as ptc
 import math
 
+def calcNewMLC(old_MLC, radius=41.5, ssd=143.5, cil=35.77-0.09):
+	
+	new_MLC = []
+
+	for j in range(len(old_MLC)):
+		if j<80:
+			if old_MLC[j]<=0:
+				new_MLC.append(((cil + math.sqrt(pow(radius,2)-pow(math.cos(abs(old_MLC[j])*0.1/ssd)*radius,2)))*abs(old_MLC[j])*0.1/ssd + math.cos(abs(old_MLC[j])*0.1/ssd)*radius)*(-1))
+			if old_MLC[j]>0:
+				new_MLC.append((-(cil - math.sqrt(pow(radius,2)-pow(math.cos(abs(old_MLC[j])*0.1/ssd)*radius,2)))*abs(old_MLC[j])*0.1/ssd + math.cos(abs(old_MLC[j])*0.1/ssd)*radius)*(-1))    
+		if j>=80:
+			if old_MLC[j]>=0:
+				new_MLC.append((cil + math.sqrt(pow(radius,2)-pow(math.cos(abs(old_MLC[j])*0.1/ssd)*radius,2)))*abs(old_MLC[j])*0.1/ssd + math.cos(abs(old_MLC[j])*0.1/ssd)*radius) 
+			if old_MLC[j]<0:
+				new_MLC.append(-(cil - math.sqrt(pow(radius,2)-pow(math.cos(abs(old_MLC[j])*0.1/ssd)*radius,2)))*abs(old_MLC[j])*0.1/ssd + math.cos(abs(old_MLC[j])*0.1/ssd)*radius)
+
+	return np.array(new_MLC)
+
+def calcNewJAW(old_JAWS, cil=44.35-0.09, radius=13.0, ssd=143.5):
+
+	new_JAWS = np.zeros(2)
+
+	if old_JAWS[0]<=0:
+	    new_JAWS[0]=((cil + math.sqrt(pow(radius,2)-pow(math.cos(abs(old_JAWS[0])*0.1/ssd)*radius,2)))*abs(old_JAWS[0])*0.1/ssd + math.cos(abs(old_JAWS[0])*0.1/ssd)*radius)*(-1)
+	if old_JAWS[0]>0:
+	    new_JAWS[0]=(-(cil - math.sqrt(pow(radius,2)-pow(math.cos(abs(old_JAWS[0])*0.1/ssd)*radius,2)))*abs(old_JAWS[0])*0.1/ssd + math.cos(abs(old_JAWS[0])*0.1/ssd)*radius)*(-1)   
+	if old_JAWS[1]>=0:
+	    new_JAWS[1]=(cil + math.sqrt(pow(radius,2)-pow(math.cos(abs(old_JAWS[1])*0.1/ssd)*radius,2)))*abs(old_JAWS[1])*0.1/ssd + math.cos(abs(old_JAWS[1])*0.1/ssd)*radius
+	if old_JAWS[1]<0:
+	    new_JAWS[1]=-(cil - math.sqrt(pow(radius,2)-pow(math.cos(abs(old_JAWS[1])*0.1/ssd)*radius,2)))*abs(old_JAWS[1])*0.1/ssd + math.cos(abs(old_JAWS[1])*0.1/ssd)*radius   
+	  
+	return new_JAWS
+
 #read in dose file for specific field size and plot it optionally
-plot = False
+plot = True
 
 dose_path = "/Users/simongutwein/Downloads/Share_Simon/DICOMData/22X22/MRI_Phantom_Reference22X22_Dose.dcm"
 ds = pydicom.read_file(dose_path,force=True)
@@ -38,63 +71,34 @@ plt.bar(field,MLC_bottom,color="w")
 plt.bar(field,-15-MLC_bottom,width=0.6,bottom=MLC_bottom)
 
 JAWS = plan.BeamSequence[0].ControlPointSequence[0].BeamLimitingDevicePositionSequence[0].LeafJawPositions
-JAW_left = np.array(JAWS[0])
-JAW_right = np.array(JAWS[1])
-JAW_right.astype(float)
-JAW_left.astype(float)
-#print(JAW_left, JAW_right)
+JAWS = np.array(JAWS)
+print(JAWS)
 
+new_MLCs = calcNewMLC(MLC)
+new_JAWs = calcNewJAW(JAWS)
+#print(new_MLCs)
+#print(new_JAWs)
 #plt.vlines(57.2/2+JAW_left/10.0,-15,15,color="green")
 #plt.vlines(57.2/2+JAW_right/10.0,-15,15,color="green")
+
 ax = plt.gca()
-ax.add_patch(ptc.Rectangle((0,-15),57.2/2+JAW_left/10.0,30,facecolor="g",alpha=0.7))
-ax.add_patch(ptc.Rectangle((57.2/2+JAW_right/10.0,-15),57.2/2-JAW_right/10.0,30,facecolor="g",alpha=0.7))
+ax.add_patch(ptc.Rectangle((0,-15),57.2/2+JAWS[0]/10.0,30,facecolor="g",alpha=0.7))
+ax.add_patch(ptc.Rectangle((57.2/2+JAWS[1]/10.0,-15),57.2/2-JAWS[1]/10.0,30,facecolor="g",alpha=0.7))
 
 plt.axis('equal')
 plt.tight_layout()
 if plot == True:
 	plt.show()
 
-r = 41.5
-ssd =143.5
-cil = 35.77-0.09
-
-MLC_new = []
-
-for j in range(len(MLC)):
-    if j<80:
-        if MLC[j]<=0:
-            MLC_new.append(((cil + math.sqrt(pow(r,2)-pow(math.cos(abs(MLC[j])*0.1/ssd)*r,2)))*abs(MLC[j])*0.1/ssd + math.cos(abs(MLC[j])*0.1/ssd)*r)*(-1))
-        if MLC[j]>0:
-            MLC_new.append((-(cil - math.sqrt(pow(r,2)-pow(math.cos(abs(MLC[j])*0.1/ssd)*r,2)))*abs(MLC[j])*0.1/ssd + math.cos(abs(MLC[j])*0.1/ssd)*r)*(-1))    
-    
-    if j>=80:
-        if MLC[j]>=0:
-            MLC_new.append((cil + math.sqrt(pow(r,2)-pow(math.cos(abs(MLC[j])*0.1/ssd)*r,2)))*abs(MLC[j])*0.1/ssd + math.cos(abs(MLC[j])*0.1/ssd)*r) 
-        if MLC[j]<0:
-            MLC_new.append(-(cil - math.sqrt(pow(r,2)-pow(math.cos(abs(MLC[j])*0.1/ssd)*r,2)))*abs(MLC[j])*0.1/ssd + math.cos(abs(MLC[j])*0.1/ssd)*r)     
- 
-cil_jaw = 44.35-0.09
-r_jaw = 13.0
-
-if JAW_left<=0:
-    Jaw_new_left=((cil_jaw + math.sqrt(pow(r_jaw,2)-pow(math.cos(abs(JAW_left)*0.1/ssd)*r_jaw,2)))*abs(JAW_left)*0.1/ssd + math.cos(abs(JAW_left)*0.1/ssd)*r_jaw)*(-1)
-if JAW_left>0:
-    Jaw_new_left=(-(cil_jaw - math.sqrt(pow(r_jaw,2)-pow(math.cos(abs(JAW_left)*0.1/ssd)*r_jaw,2)))*abs(JAW_left)*0.1/ssd + math.cos(abs(JAW_left)*0.1/ssd)*r_jaw)*(-1)   
-if JAW_right>=0:
-    Jaw_new_right=(cil_jaw + math.sqrt(pow(r_jaw,2)-pow(math.cos(abs(JAW_right)*0.1/ssd)*r_jaw,2)))*abs(JAW_right)*0.1/ssd + math.cos(abs(JAW_right)*0.1/ssd)*r_jaw
-if JAW_right<0:
-    Jaw_new_right=-(cil_jaw - math.sqrt(pow(r_jaw,2)-pow(math.cos(abs(JAW_right)*0.1/ssd)*r_jaw,2)))*abs(JAW_right)*0.1/ssd + math.cos(abs(JAW_right)*0.1/ssd)*r_jaw   
-  
-MLC_new = np.array(MLC_new)
+#MLC_new = np.array(MLC_new)
 
 if plot == True:
 	plt.figure()  
-	plt.plot(MLC_new[:80]+41.5)
-	plt.plot(MLC_new[80:]-41.5)
+	plt.plot(new_MLCs[:80]+41.5)
+	plt.plot(new_MLCs[80:]-41.5)
 	plt.show()
 
-print("MLC Positions Top MLC:",MLC_new[:80].shape, "\nMLC Positions Bottom MLC:", MLC_new[80:].shape,"\nNew Jaw-Position Left Jaw:", Jaw_new_left, "\nNew Jaw-Position Right Jaw:", Jaw_new_right)
+print("MLC Positions Top MLC:",new_MLCs[:80].shape, "\nMLC Positions Bottom MLC:", new_MLCs[80:].shape,"\nNew Jaw-Position Left Jaw:", new_JAWs[0], "\nNew Jaw-Position Right Jaw:", new_JAWs[1])
 
 data= ds.pixel_array
 
