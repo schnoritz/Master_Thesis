4925bb07498113664f02ded0f3e0ada45d17dc34
Simon Gutwein <simon.bonvino@gmail.com>
2021-02-27 15:45:37 +0100

    Code into Functions

--- Code/DICOM_read_dose.py
+++ Code/DICOM_read_dose.py
@@ -6,19 +6,20 @@ import math
 
 def calcNewMLC(old_MLC, radius=41.5, ssd=143.5, cil=35.77-0.09):
 	
-	new_MLC = []
+	new_MLC = np.zeros((2,80))
 
-	for j in range(len(old_MLC)):
-		if j<80:
-			if old_MLC[j]<=0:
-				new_MLC.append(((cil + math.sqrt(pow(radius,2)-pow(math.cos(abs(old_MLC[j])*0.1/ssd)*radius,2)))*abs(old_MLC[j])*0.1/ssd + math.cos(abs(old_MLC[j])*0.1/ssd)*radius)*(-1))
-			if old_MLC[j]>0:
-				new_MLC.append((-(cil - math.sqrt(pow(radius,2)-pow(math.cos(abs(old_MLC[j])*0.1/ssd)*radius,2)))*abs(old_MLC[j])*0.1/ssd + math.cos(abs(old_MLC[j])*0.1/ssd)*radius)*(-1))    
-		if j>=80:
-			if old_MLC[j]>=0:
-				new_MLC.append((cil + math.sqrt(pow(radius,2)-pow(math.cos(abs(old_MLC[j])*0.1/ssd)*radius,2)))*abs(old_MLC[j])*0.1/ssd + math.cos(abs(old_MLC[j])*0.1/ssd)*radius) 
-			if old_MLC[j]<0:
-				new_MLC.append(-(cil - math.sqrt(pow(radius,2)-pow(math.cos(abs(old_MLC[j])*0.1/ssd)*radius,2)))*abs(old_MLC[j])*0.1/ssd + math.cos(abs(old_MLC[j])*0.1/ssd)*radius)
+	for j in range(2):
+		for i in range(80):
+			if j == 0:
+				if old_MLC[j,i]<=0:
+					new_MLC[j,i] = (((cil + math.sqrt(pow(radius,2)-pow(math.cos(abs(old_MLC[j,i])*0.1/ssd)*radius,2)))*abs(old_MLC[j,i])*0.1/ssd + math.cos(abs(old_MLC[j,i])*0.1/ssd)*radius)*(-1))
+				if old_MLC[j,i]>0:
+					new_MLC[j,i] = ((-(cil - math.sqrt(pow(radius,2)-pow(math.cos(abs(old_MLC[j,i])*0.1/ssd)*radius,2)))*abs(old_MLC[j,i])*0.1/ssd + math.cos(abs(old_MLC[j,i])*0.1/ssd)*radius)*(-1))    
+			else:
+				if old_MLC[j,i]>=0:
+					new_MLC[j,i] = ((cil + math.sqrt(pow(radius,2)-pow(math.cos(abs(old_MLC[j,i])*0.1/ssd)*radius,2)))*abs(old_MLC[j,i])*0.1/ssd + math.cos(abs(old_MLC[j,i])*0.1/ssd)*radius) 
+				if old_MLC[j,i]<0:
+					new_MLC[j,i] = (-(cil - math.sqrt(pow(radius,2)-pow(math.cos(abs(old_MLC[j,i])*0.1/ssd)*radius,2)))*abs(old_MLC[j,i])*0.1/ssd + math.cos(abs(old_MLC[j,i])*0.1/ssd)*radius)
 
 	return np.array(new_MLC)
 
@@ -37,13 +38,32 @@ def calcNewJAW(old_JAWS, cil=44.35-0.09, radius=13.0, ssd=143.5):
 	  
 	return new_JAWS
 
+def plot_MLC_field(MLC_dat, JAWS_dat):
+
+	MLC_dat = MLC_dat/10
+	field = np.linspace(0,0.715*80,80)
+	plt.bar(field,MLC_dat[1,:],color="w")
+	plt.bar(field,15-MLC_dat[1,:],width=0.6,bottom=MLC_dat[1,:])
+	plt.bar(field,MLC_dat[0,:],color="w")
+	plt.bar(field,-15-MLC_dat[0,:],width=0.6,bottom=MLC_dat[0,:])
+
+	ax = plt.gca()
+	ax.add_patch(ptc.Rectangle((0,-15),57.2/2+JAWS_dat[0]/10.0,30,facecolor="g",alpha=0.7))
+	ax.add_patch(ptc.Rectangle((57.2/2+JAWS_dat[1]/10.0,-15),57.2/2-JAWS_dat[1]/10.0,30,facecolor="g",alpha=0.7))
+
+	plt.axis('equal')
+	plt.tight_layout()
+	plt.show()
+
+	return
+
 #read in dose file for specific field size and plot it optionally
 plot = True
 
 dose_path = "/Users/simongutwein/Downloads/Share_Simon/DICOMData/22X22/MRI_Phantom_Reference22X22_Dose.dcm"
 ds = pydicom.read_file(dose_path,force=True)
 
-plan_path = "/Users/simongutwein/Downloads/Share_Simon/DICOMData/15X15/MRI_Phantom_Reference15X15.dcm"
+plan_path = "/Users/simongutwein/Downloads/Share_Simon/DICOMData/10X10/MRI_Phantom_Reference10X10.dcm"
 plan = pydicom.read_file(plan_path,force=True)
 
 #print(plan)
@@ -51,52 +71,21 @@ plan = pydicom.read_file(plan_path,force=True)
 #Maximale Öffnungsposition ist -110, geschlossen ist -2
 # -> Angaben sind in mm -> -110 sind 11cm -> Bei beiden MLC bei -110 und +110 also Feldöffnunf von 220mm 
 # -> Also 22cm
-MLC = plan.BeamSequence[0].ControlPointSequence[0].BeamLimitingDevicePositionSequence[1].LeafJawPositions
-MLC_bottom = np.array(MLC[:80])/10
-MLC_Top = np.array(MLC[80:])/10
-MLC_Top.astype(float)
-MLC_bottom.astype(float)
-MLC = np.array(MLC)
-MLC.astype(float)
-print("TOP =", len(MLC_bottom), "\nBOTTOM =", len(MLC_Top))
-#print(MLC_bottom)
-
-field = np.linspace(0,0.715*80,80)
-
-#plt.plot(field,MLC_Top)
-#plt.plot(field,MLC_bottom)
-plt.bar(field,MLC_Top,color="w")
-plt.bar(field,15-MLC_Top,width=0.6,bottom=MLC_Top)
-plt.bar(field,MLC_bottom,color="w")
-plt.bar(field,-15-MLC_bottom,width=0.6,bottom=MLC_bottom)
+MLC = np.array(plan.BeamSequence[0].ControlPointSequence[0].BeamLimitingDevicePositionSequence[1].LeafJawPositions).astype(float)
+MLC = MLC.reshape((2,80))
+print("TOP =", len(MLC[0,:]), "\nBOTTOM =", len(MLC[1,:]))
+print(MLC)
 
 JAWS = plan.BeamSequence[0].ControlPointSequence[0].BeamLimitingDevicePositionSequence[0].LeafJawPositions
 JAWS = np.array(JAWS)
-print(JAWS)
 
 new_MLCs = calcNewMLC(MLC)
 new_JAWs = calcNewJAW(JAWS)
-#print(new_MLCs)
+print(new_MLCs)
 #print(new_JAWs)
-#plt.vlines(57.2/2+JAW_left/10.0,-15,15,color="green")
-#plt.vlines(57.2/2+JAW_right/10.0,-15,15,color="green")
-
-ax = plt.gca()
-ax.add_patch(ptc.Rectangle((0,-15),57.2/2+JAWS[0]/10.0,30,facecolor="g",alpha=0.7))
-ax.add_patch(ptc.Rectangle((57.2/2+JAWS[1]/10.0,-15),57.2/2-JAWS[1]/10.0,30,facecolor="g",alpha=0.7))
-
-plt.axis('equal')
-plt.tight_layout()
-if plot == True:
-	plt.show()
-
-#MLC_new = np.array(MLC_new)
 
 if plot == True:
-	plt.figure()  
-	plt.plot(new_MLCs[:80]+41.5)
-	plt.plot(new_MLCs[80:]-41.5)
-	plt.show()
+	plot_MLC_field(MLC, JAWS)
 
 print("MLC Positions Top MLC:",new_MLCs[:80].shape, "\nMLC Positions Bottom MLC:", new_MLCs[80:].shape,"\nNew Jaw-Position Left Jaw:", new_JAWs[0], "\nNew Jaw-Position Right Jaw:", new_JAWs[1])
 
