a77b216e8bc883471748db3a84ddd880512c1e14
Simon Gutwein <simon.bonvino@gmail.com>
2021-02-27 22:24:43 +0100

    Moved Functions here

--- Code/functions.py
+++ Code/functions.py
@@ -17,9 +17,6 @@ def plot_MLC_field(MLC_dat, JAWS_dat):
 	plt.tight_layout()
 	plt.show()
 
-	plt.draw()
-	plt.pause(0.001)
-
 	return
 
 def calcNewMLC(old_MLC, radius=41.5, ssd=143.5, cil=35.77-0.09):
@@ -55,5 +52,81 @@ def calcNewJAW(old_JAWS, cil=44.35-0.09, radius=13.0, ssd=143.5):
 	    new_JAWS[1]=-(cil - math.sqrt(pow(radius,2)-pow(math.cos(abs(old_JAWS[1])*0.1/ssd)*radius,2)))*abs(old_JAWS[1])*0.1/ssd + math.cos(abs(old_JAWS[1])*0.1/ssd)*radius   
 	  
 	return new_JAWS
+
+def create_field_parameters(size=None, translation=None):
+	"""creates parameters for the field, size and offset can be randomly generated. 
+	if size and translation are given this code produces the wanted field parameters
+	size and translation are tuples with x-dim and y-dim of field and translation
+	"""
+	if size is None:
+		fieldsize = np.array([random.randint(2,57), random.randint(2,22)])
+
+	else:
+		fieldsize = np.array([size[0], size[1]])
+
+	#print("FIELDSIZE:", fieldsize)
+
+	if translation is None:
+		max_x_translation = 28.5-fieldsize[0]/2
+		max_y_translation = 11-fieldsize[1]/2
+		#print("MAXMIMUM OFFSET VALUES:", max_x_translation, max_y_translation)
+		#accounting for boundary conditions that field translation can't be so that field lies outside of maximum field
+		#offset = [random.uniform(-max_x_translation, max_x_translation), random.uniform(-max_y_translation, max_y_translation)]
+		offset = [random.randint(-np.floor(max_x_translation), np.floor(max_x_translation)), random.randint(-np.floor(max_y_translation), np.floor(max_y_translation))]
+	else:
+		offset = [translation[0],translation[1]]
+
+	return fieldsize, offset
+
+def calculate_MLC_positions(fieldsize, offset):
+	
+	MLC = np.zeros((2,80))
+	dx, dy = offset[0], offset[1]
+	central_leafes = [np.floor(dx/0.715).astype(int) + 40, np.floor(dx/0.715).astype(int) + 41]
+	#print("CENTRAL_LEAFES:", central_leafes)
+
+	#anzahl der leafes die noch hinzugefügt werden sollen, in dem Fall hier 4 Leafes mehr als benötigt werden.
+	count_leafes = np.ceil(fieldsize[0]/0.715).astype(int)+2
+	
+	#Leaf anzahl immer gerade machen
+	if count_leafes%2 != 0:
+		count_leafes += 1
+	#print("COUT_LEAFES:", count_leafes)
+
+	MLC[0,:] += dy - 0.2 #Spalt in der Mitte erzeugen, 0,2mm breite
+	MLC[1,:] += dy + 0.2
+
+	if int(central_leafes[0]-count_leafes/2)-1 < 0 and int(central_leafes[1]+count_leafes/2) < 80:
+
+		MLC[0,0:central_leafes[0]] -= fieldsize[1]/2
+		MLC[1,0:central_leafes[0]] += fieldsize[1]/2
+		MLC[0,central_leafes[1]-1:int(central_leafes[1]+count_leafes/2)] -= fieldsize[1]/2
+		MLC[1,central_leafes[1]-1:int(central_leafes[1]+count_leafes/2)] += fieldsize[1]/2
+	
+	elif int(central_leafes[1]+count_leafes/2) >= 80 and int(central_leafes[0]-count_leafes/2)-1 > 0:
+		
+		MLC[0,int(central_leafes[0]-count_leafes/2)-1:central_leafes[0]] -= fieldsize[1]/2
+		MLC[1,int(central_leafes[0]-count_leafes/2)-1:central_leafes[0]] += fieldsize[1]/2
+		MLC[0,central_leafes[1]-1:80] -= fieldsize[1]/2
+		MLC[1,central_leafes[1]-1:80] += fieldsize[1]/2
+	
+	elif int(central_leafes[1]+count_leafes/2) >= 80 and int(central_leafes[0]-count_leafes/2)-1 < 0:
+
+		MLC[0,0:central_leafes[0]] -= fieldsize[1]/2
+		MLC[1,0:central_leafes[0]] += fieldsize[1]/2
+		MLC[0,central_leafes[1]-1:80] -= fieldsize[1]/2
+		MLC[1,central_leafes[1]-1:80] += fieldsize[1]/2
+
+	else:
+
+		MLC[0,int(central_leafes[0]-count_leafes/2)-1:central_leafes[0]] -= fieldsize[1]/2
+		MLC[1,int(central_leafes[0]-count_leafes/2)-1:central_leafes[0]] += fieldsize[1]/2
+		MLC[0,central_leafes[1]-1:int(central_leafes[1]+count_leafes/2)] -= fieldsize[1]/2
+		MLC[1,central_leafes[1]-1:int(central_leafes[1]+count_leafes/2)] += fieldsize[1]/2
+
+	#JAW positionen berechnen
+	JAWS = np.array([dx-fieldsize[0]/2, dx+fieldsize[0]/2])*10.0
+
+	return MLC, JAWS
 
 	
