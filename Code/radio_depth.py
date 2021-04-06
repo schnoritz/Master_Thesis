223f7dadf5a1594472bc5dd67c0079454f4965ec
Simon Gutwein <simon.bonvino@gmail.com>
2021-04-06 19:23:02 +0200

    Problems on certain angles
    Dont know why

--- Code/radio_depth.py
+++ Code/radio_depth.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import math
-from numba import njit
+from numba import njit, prange
 import matplotlib.pyplot as plt
 from time import time
 
@@ -18,183 +18,133 @@ class ray():
 
     def calculate_path(self):
 
-        x, y, z = self.voxel_pos[0], self.voxel_pos[1], self.voxel_pos[2]
-        if self.ray_vector[0] == 0:
-            if self.ray_vector[1] > 1:
-                depth = np.sum(self.target_volume[x, 0:y,z])
-            else:
-                depth = np.sum(self.target_volume[x, y:self.target_volume.shape[1], z])
+        a_min= calculate_a(self.target_volume, self.origin_pos, self.ray_vector)
+        a_max = 1
 
-        elif self.ray_vector[1] == 0:
-            if self.ray_vector[0] > 1:
-                depth = np.sum(self.target_volume[0:x, y, z])
-            else:
-                depth = np.sum(self.target_volume[x:self.target_volume.shape[0], y, z])
-
-        else:
-
-            a_min, plane= calculate_a(self.target_volume, self.origin_pos, self.voxel_pos)
-
-            a_max = 1
-            i_x_min, i_x_max, i_y_min, i_y_max = get_min_max_index(self.target_volume, self.origin_pos, self.ray_vector, plane, a_min)
-            a = calc_alpha(self.origin_pos, self.voxel_pos, i_x_min, i_x_max, i_y_min, i_y_max, a_min, a_max)
-            depth = calc_depth(self.origin_pos, self.ray_vector, a, self.target_volume)
+        i_x_min, i_x_max, i_y_min, i_y_max, i_z_min, i_z_max = get_min_max_index(self.target_volume, self.origin_pos, self.ray_vector, a_min)
+        
+        a = calc_alpha(self.origin_pos, self.voxel_pos, i_x_min, i_x_max, i_y_min, i_y_max, i_z_min, i_z_max, a_min, a_max)
+        depth = calc_depth(self.origin_pos, self.ray_vector, a, self.target_volume)
             
         return depth
 
 
-    # def calculate_a(self):
+@njit(fastmath=True)
+def get_min_max_index(target, origin, ray_vec, a_min):
 
-    #     volume_dim = self.target_volume.shape
-
-    #     a_x = np.array([(self.origin_pos[0]-0)/(self.origin_pos[0]-self.voxel_pos[0]),
-    #               (self.origin_pos[0]-volume_dim[0])/(self.origin_pos[0]-self.voxel_pos[0])])
-        
-    #     a_x = a_x[a_x > 0].min()
-
-    #     a_y = np.array([(self.origin_pos[1]-0)/(self.origin_pos[1]-self.voxel_pos[1]),
-    #                     (self.origin_pos[1]-volume_dim[1])/(self.origin_pos[1]-self.voxel_pos[1])])
-
-    #     a_y = a_y[a_y > 0].min()
-
-    #     a_min = np.array([a_x, a_y]).min()
-
-    #     if a_y < a_x:
-    #         plane = 'y'
-    #     else:
-    #         plane = 'x'
-        
-    #     return a_min, plane
-
-    # def get_min_max_index(self,plane, a_min):
-
-    #     if plane == 'x':
-    #             i_x_min = 1
-    #             i_x_max = self.target_volume.shape[0]
-    #             i_y_min = np.floor(
-    #                 self.origin_pos[1] + self.ray_vector[1]).astype('int32')
-    #             i_y_max = np.ceil(
-    #                 self.origin_pos[1] + a_min*self.ray_vector[1]).astype('int32')
-
-    #     else:
-    #         i_y_min = 1
-    #         i_y_max = self.target_volume.shape[0]
-    #         i_x_min = np.floor(self.origin_pos[0] + self.ray_vector[0]).astype('int32')
-    #         i_x_max = np.ceil(
-    #             self.origin_pos[0] + a_min*self.ray_vector[0]).astype('int32')
-
-    #     return i_x_min, i_x_max, i_y_min, i_y_max
-
-    # def calc_alpha(self, i_x_min, i_x_max, i_y_min, i_y_max, a_min, a_max):
-
-    #     a_x = []
-    #     for i in range(i_x_min, i_x_max+1):
-    #         a_x.append((self.origin_pos[0]-i)/(self.origin_pos[0]-self.voxel_pos[0]))
-
-    #     a_y = []
-    #     for i in range(i_y_min, i_y_max+1):
-    #         a_y.append((self.origin_pos[1]-i)/(self.origin_pos[1]-self.voxel_pos[1]))
-
-    #     a_x, a_y = np.array(a_x), np.array(a_y)
-    #     a = np.concatenate([a_x, a_y])
-    #     a = a[a <= a_max]
-    #     a = a[a >= a_min]
-    #     a = np.unique(np.sort(a))
-
-    #     return a
-
-    # def calc_depth(self, a):
-    #     intersections = np.array([self.origin_pos + alpha * self.ray_vector for alpha in a])
-    #     diff = np.array([intersections[i] - intersections[i+1]for i in range(len(intersections)-1)])
-    #     inter = [zip(np.ceil((intersections[i]+intersections[i+1]) /2).astype('int32')-1) for i in range(len(intersections)-1)]
-
-    #     lengths = np.array(
-    #         [np.sqrt(np.square(var[0]) + np.square(var[1]) + np.square(var[2])) for var in diff])
-
-    #     depth = np.sum(np.array(
-    #         [self.target_volume[x, y, z] * lengths[i] for i, (x, y, z) in enumerate(inter)]))
-
-    #     return depth
-
-@njit
-def get_min_max_index(target, origin, ray_vec, plane, a_min):
-
-    if plane == 'x':
-        i_x_min = 1
-        i_x_max = target.shape[0]
-        i_y_min = int(np.floor(origin[1] + ray_vec[1]))
-        i_y_max = int(np.ceil(origin[1] + a_min*ray_vec[1]))
-
+    if ray_vec[0] < 0:
+        i_x_min = int(np.ceil(origin[0] + ray_vec[0]))
+        i_x_max = int(np.floor(origin[0] + a_min*ray_vec[0]))
+    elif ray_vec[0] > 0:
+        i_x_min = int(np.floor(origin[0] + a_min*ray_vec[0]))
+        i_x_max = int(np.ceil(origin[0] + ray_vec[0]))
     else:
-        i_y_min = 1
-        i_y_max = target.shape[0]
-        i_x_min = int(np.floor(origin[0] + ray_vec[0]))
-        i_x_max = int(np.ceil(origin[0] + a_min*ray_vec[0]))
+        i_x_min, i_x_max = -1, -1
 
-    return i_x_min, i_x_max, i_y_min, i_y_max
 
-@njit
-def calc_alpha(origin, voxel, i_x_min, i_x_max, i_y_min, i_y_max, a_min, a_max):
+    if ray_vec[1] < 0:
+        i_y_min = int(np.ceil(origin[1] + ray_vec[1]))
+        i_y_max = int(np.floor(origin[1] + a_min*ray_vec[1]))
+    elif ray_vec[1] > 0:
+        i_y_min = int(np.floor(origin[1] + a_min*ray_vec[1]))
+        i_y_max = int(np.ceil(origin[1] + ray_vec[1]))
+    else:
+        i_y_min, i_y_max = -1, -1
+    
+    if ray_vec[2] < 0:
+        i_z_min = int(np.ceil(origin[2] + ray_vec[2]))
+        i_z_max = int(np.floor(origin[2] + a_min*ray_vec[2]))
+    elif ray_vec[2] > 0:
+        i_z_min = int(np.floor(origin[2] + a_min*ray_vec[2]))
+        i_z_max = int(np.ceil(origin[2] + ray_vec[2]))
+    else:
+        i_z_min, i_z_max = -1, -1
+
+
+    return i_x_min, i_x_max, i_y_min, i_y_max, i_z_min, i_z_max
+
+
+@njit(fastmath=True)
+def calc_alpha(origin, voxel, i_x_min, i_x_max, i_y_min, i_y_max, i_z_min, i_z_max, a_min, a_max):
 
     a_x = []
-    for i in range(i_x_min, i_x_max+1):
-        a_x.append((origin[0]-i)/(origin[0]-voxel[0]))
+    if not i_x_min == -1:
+        for i in range(i_x_min, i_x_max+1):
+            a_x.append((origin[0]-i)/(origin[0]-voxel[0]))
 
     a_y = []
-    for i in range(i_y_min, i_y_max+1):
-        a_y.append((origin[1]-i)/(origin[1]-voxel[1]))
+    if not i_y_min == -1:
+        for i in range(i_y_min, i_y_max+1):
+            a_y.append((origin[1]-i)/(origin[1]-voxel[1]))
 
-    a_x, a_y = np.array(a_x), np.array(a_y)
-    a = np.concatenate((a_x, a_y))
+    a_z = []
+    if not i_z_min == -1:
+        for i in range(i_z_min, i_z_max+1):
+            a_z.append((origin[2]-i)/(origin[2]-voxel[2]))
+
+    a_x, a_y, a_z = np.array(a_x), np.array(a_y), np.array(a_z)
+    a = np.concatenate((a_x, a_y, a_z))
     a = a[a <= a_max]
     a = a[a >= a_min]
     a = np.unique(np.sort(a))
-
+    
     return a
 
-@njit #code noch debuggen für parallelisierung
-def calculate_a(target, origin, voxel):
 
-    volume_dim = target.shape
+@njit(fastmath=True)  # code noch debuggen für parallelisierung
+def calculate_a(target, origin, ray_vec):
 
-    a_x = np.array([(origin[0]-0)/(origin[0]-voxel[0]),
-                    (origin[0]-volume_dim[0])/(origin[0]-voxel[0])])
+    a_min = 0
+    plane = 'N'
+    volume_dim = np.array(target.shape)-1
+
+    if ray_vec[0] == 0:
+        a_x = np.array([np.inf, np.inf])
+    else:
+        a_x = np.array([(0-origin[0])/(ray_vec[0]),
+                        (volume_dim[0]-origin[0])/(ray_vec[0])])
 
     a_x = a_x[a_x > 0].min()
 
-    a_y = np.array([(origin[1]-0)/(origin[1]-voxel[1]),
-                     (origin[1]-volume_dim[1])/(origin[1]-voxel[1])])
+    if ray_vec[1] == 0:
+        a_y = np.array([np.inf, np.inf])
+    else:
+        a_y = np.array([(0-origin[1])/(ray_vec[1]),
+                        (volume_dim[1]-origin[1])/(ray_vec[1])])
 
     a_y = a_y[a_y > 0].min()
 
-    a_min = np.array([a_x, a_y]).min()
-
-    if a_y < a_x:
-        plane = 'y'
+    if origin[0] + a_y * ray_vec[0] > volume_dim[0] or origin[0] + a_y * ray_vec[0] < 0:
+        a_min = a_x
+    elif origin[1] + a_x * ray_vec[1] > volume_dim[1] or origin[1] + a_x * ray_vec[1] < 0:
+        a_min = a_y
     else:
-        plane = 'x'
+        a_min = np.array([a_x, a_y]).min()
+    
+    return a_min
 
-    return a_min, plane
 
-@njit
+@njit(fastmath=True)#, parallel=True)
 def calc_depth(origin, ray_vector, a, target_volume):
     intersections = np.empty((len(a),3))
     diff = np.empty((intersections.shape[0]-1,intersections.shape[1]))
     lengths = np.empty(diff.shape[0])
     depth = 0.0
 
-    for i in range(len(a)):
+    for i in prange(len(a)):
         intersections[i,:] = origin + a[i]*ray_vector
     
-    inter = np.ceil(intersections[:-1])-1
-
-    for i in range(len(intersections)-1):
+    inter = np.ceil(intersections[:-1])#-1
+    # with np.printoptions(threshold=np.inf):
+    #     print(np.round(intersections,1))
+   
+    for i in prange(len(intersections)-1):
         diff[i] = intersections[i] - intersections[i+1]
 
-    for i in range(len(diff)):
+    for i in prange(len(diff)):
         lengths[i] = np.sqrt(np.square(diff[i][0]) + np.square(diff[i][1]) + np.square(diff[i][2]))
 
-    for i in range(len(lengths)):
+    for i in prange(len(lengths)):
         depth += lengths[i] * target_volume[int(inter[i][0]), int(inter[i][1]), int(inter[i][2])]   
 
     return depth
@@ -206,32 +156,42 @@ def rgb2gray(rgb):
 
     return gray
 
+def debug(rotate=True):
+
+    if rotate==True:
+        angle = np.linspace(0, 1, 10, endpoint=False)*2*np.pi
+        x = 800*np.cos(angle)
+        y = 800*np.sin(angle)
+        origin = []
+        for i,j in zip(x,y):
+            origin.append([i,j,20])
+
+        origins = np.array(origin)
+
+    return origins
+
+
 if __name__ == "__main__":
     
-    origin = np.array([-10,510,25])
-    volume = plt.imread("/Users/simongutwein/Studium/Masterarbeit/CT_512.jpg")
-    volume = np.resize(volume, (32, volume.shape[0], volume.shape[1]))
+    volume = np.array(plt.imread("/Users/simongutwein/Studium/Masterarbeit/CT_512.jpg")).T
+    volume = np.resize(volume, (64, volume.shape[0], volume.shape[1]))
     volume = volume.transpose(1, 2, 0)
-
     radio_depth_volume = np.empty((volume.shape[0], volume.shape[1]))
-    needed_time = []
-    start = time()
-    for i in range(volume.shape[0]):
-        for j in range(volume.shape[1]):
-            voxel = np.array([i, j, 20])
-            curr_ray = ray(origin, voxel, volume)
-            radio_depth_volume[i, j] = curr_ray.path
-            
-            
-    needed_time = time()-start
-    time_per_voxel = needed_time/(volume.shape[0]*volume.shape[1])
-    print("Needed Time (all): ", needed_time, "\nTime per Voxel: ", time_per_voxel)
 
-    plt.imshow(radio_depth_volume, cmap="magma")
-    plt.imshow(volume[:, :, 0], cmap="gray", alpha=0.3)
-    plt.show()
+    for origin in debug():
+        print(origin)
+        needed_time = []
+        start = time()
+        for i in range(volume.shape[0]):
+            for j in range(volume.shape[1]):
+                voxel = np.array([i, j, 20])
+                curr_ray = ray(origin, voxel, volume)
+                radio_depth_volume[i, j] = curr_ray.path
+                
+        needed_time = time()-start
+        time_per_voxel = needed_time/(volume.shape[0]*volume.shape[1])
+        print("Needed Time (all): ", needed_time, "\nTime per Voxel: ", time_per_voxel)
 
-               
-
-    
-    
+        plt.imshow(radio_depth_volume, cmap="magma")
+        plt.imshow(volume[:, :, 50], cmap="gray", alpha=0.3, origin='lower')
+        plt.show()
