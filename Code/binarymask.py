3c925306ffeda3f5811ce55a8148367e6c339105
Simon Gutwein <simon.bonvino@gmail.com>
2021-05-03 15:58:43 +0200

    Function for artificial fields and dcm

--- Code/binarymask.py
+++ Code/binarymask.py
@@ -24,11 +24,11 @@ def get_leaf_positions(path):
         for line in lines:
             positions.append(np.array(line.split(",")[:2], dtype=float))
 
-    if 'jaws' in locals():
-        return positions, jaws
-    else: 
-        return np.array(positions), None
+        jaws = np.array(positions[-1])*10
 
+        positions = positions[:-1]
+
+    return positions, jaws
 
 
 def get_first_layer(positions, jaws, slice_increase, transversal_size, sagital_size, dim):
@@ -47,9 +47,6 @@ def get_first_layer(positions, jaws, slice_increase, transversal_size, sagital_s
             fl[:219+int(np.round(top*20)), i] = 0
         if bot != 0:
             fl[219+int(np.round(bot*20)):, i] = 0
-
-    plt.imshow(resize(fl, (400, 400)))
-    plt.show()
 
     fl = resize(fl, (transversal_size, sagital_size))
     l_jaw_idx = sagital_size//2 + int(np.round(jaws[0]))
@@ -81,31 +78,20 @@ def scale_beam(fl, dim, scale):
 def clipped_zoom(img, scale):
 
     h, w = img.shape[:2]
-    zoom_tuple = (scale, scale)
-
-    zh = int(np.ceil(h / scale))
-    zw = int(np.ceil(w / scale))
-    top = (h - zh) // 2
-    left = (w - zw) // 2
-
-    out = ndimage.zoom(img[top:top+zh, left:left+zw],
-                       zoom_tuple, order=1, prefilter=False)
-
-    trim_top = ((out.shape[0] - h) // 2)
-    trim_left = ((out.shape[1] - w) // 2)
-
-    out = out[trim_top:trim_top+h, trim_left:trim_left+w]
+    new_h = int(np.ceil(scale*h))
+    new_w = int(np.ceil(scale*w))
+    top = int(np.round((new_h - h) / 2,0))
+    left = int(np.round((new_w - w) / 2,0))
+    out = resize(img,(new_h, new_w))
+    out = out[top:top+h, left:left+w]
 
     return out
 
 
-if __name__ == "__main__":
+def create_binary_mask(file, angle):
 
-    #file = "/Users/simongutwein/Studium/Masterarbeit/MR-Linac_model_10x10_0x0.egsinp"
-    file = "/Users/simongutwein/Studium/Masterarbeit/MbaseMRL.dcm"
     voxel_size = [1.171875, 1.171875, 3]
     SID = 1435
-    angle = 0
 
     fieldsize_px = [int(np.round(220 / voxel_size[2])),
                     int(np.round(576 / voxel_size[0]))]
@@ -114,33 +100,36 @@ if __name__ == "__main__":
     rotated_dim = np.array(
         [1.01*np.sqrt(2*(output_dim[0]**2)), 1.01*np.sqrt(2*(output_dim[1]**2)), output_dim[2]], dtype=int)
 
-    print(rotated_dim)
-
     iso_center = np.array(
         [rotated_dim[0]/2-1, rotated_dim[0]/2-1, 54], dtype=int)
 
     increase_per_slice = abs(1 - SID/(SID - voxel_size[0]))
-    first_layer_factor = 1 - (rotated_dim[0] - iso_center[0]) * increase_per_slice
-    
+    first_layer_factor = 1 - \
+        (rotated_dim[0] - iso_center[0]) * increase_per_slice
+
     leaf_positions, jaws = get_leaf_positions(file)
-    jaws = jaws/voxel_size[0]
-    first_layer = get_first_layer(leaf_positions, jaws, first_layer_factor, fieldsize_px[0], fieldsize_px[1], rotated_dim)
-    
-    plt.imshow(first_layer)
-    plt.show()
+    jaws = jaws*voxel_size[0]
+    first_layer = get_first_layer(
+        leaf_positions, jaws, first_layer_factor, fieldsize_px[0], fieldsize_px[1], rotated_dim)
 
     beam = scale_beam(first_layer, rotated_dim, increase_per_slice)
 
-    print(beam.shape)
-
-    rotated = ndimage.rotate(beam, angle, axes=(1,2), reshape=False, prefilter=False)
+    rotated = ndimage.rotate(beam, angle, axes=(
+        1, 2), reshape=False, prefilter=False)
 
     crop = (rotated_dim - output_dim)//2
 
-    output = rotated[crop[2]:crop[2]+output_dim[2], crop[1]:crop[1]+output_dim[1], crop[0]:crop[0]+output_dim[0]]
-    output = np.transpose(output, (2,1,0))
-    for i in range(output.shape[1]):
-        plt.imshow(output[i, :, :])
-        plt.show()
+    output = rotated[crop[2]:crop[2]+output_dim[2], crop[1]
+        :crop[1]+output_dim[1], crop[0]:crop[0]+output_dim[0]]
+    output = np.transpose(output, (2, 1, 0))
 
-    print(output.shape)
+    return output
+
+if __name__ == "__main__":
+
+    file = "/Users/simongutwein/Documents/GitHub/Master_Thesis/Data/training_data/MR-Linac_model_10x10_0x0.txt"
+    #file = "/Users/simongutwein/Studium/Masterarbeit/MbaseMRL.dcm"
+
+    binary_mask = create_binary_mask(file, angle=45)
+    print(binary_mask.shape)
+    
